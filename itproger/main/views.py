c6be3d8ce761c6cecc6f0de4c8a95645a496db84
AlexGol005 <pesok1985>
2022-02-23 23:53:24 +0300

    23.02 home

--- itproger/main/views.py
+++ itproger/main/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'main list11',
+        'values': ['some', 'hello', 'hahaha'],
+        'obj': {
+            'car': 'bmw',
+            'age': 18,
+            'hobby': 'biser'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
